586b153e8ee878d9921dda23abe134b0f9afab65
Ctyzan <osnovaa4tech@gmail.com>
2022-07-11 13:01:31 +0300

    add esp code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests, json, time
 
+url = 'http://192.168.1.109/json'
+cooldown = 5
+
 while True:
-    str_data = requests.get('http://192.168.1.109/json', timeout=5).text
+    str_data = requests.get(url, timeout=10).text
     data = json.loads(str_data)
 
     current_time = {'time': str(round(time.time(), 2))}
@@ -11,4 +14,4 @@ while True:
         f.write(str(data) + '\n')
 
     print(data)
-    time.sleep(5)
+    time.sleep(cooldown)
