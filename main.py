231eb7824c9ad7fa874b07017361838d6d7f6541
Ctyzan <osnovaa4tech@gmail.com>
2022-07-11 14:45:24 +0300

    +matplot

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import requests, json, time
 url = 'http://192.168.1.109/json'
 cooldown = 5
 
-while True:
-    str_data = requests.get(url, timeout=10).text
-    data = json.loads(str_data)
 
-    current_time = {'time': str(round(time.time(), 2))}
-    data.update(current_time)
+if __name__ == '__main__':
+    while True:
+        str_data = requests.get(url, timeout=10).text
+        data = json.loads(str_data)
+        if data['t'] != 'nan' and data['h'] != 'nan':
+            current_time = {'time': str(round(time.time(), 2))}
+            data.update(current_time)
 
-    with open('data.txt', mode='a') as f:
-        f.write(str(data) + '\n')
+            with open('data.txt', mode='a') as f:
+                f.write(str(data) + '\n')
 
-    print(data)
-    time.sleep(cooldown)
+            print(data)
+        time.sleep(cooldown)
---
+++ timeline.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import json
+from datetime import datetime
+
+period = 300
+cooldown = 5 #set your cooldown from main script
+#from main import cooldown
+
+with open('data.txt', 'r') as f:
+    data = f.read().replace('\'', '"')
+
+splitted_data = data.split('\n')
+splitted_data.remove('')
+
+thinned_data = []
+
+for i in range(len(splitted_data)):
+    if i % (period // cooldown) == 0:
+        thinned_data.append(splitted_data[i])
+
+
+print('Lenght before:', len(splitted_data), '\nLenght after:', len(thinned_data))
+
+
+list_data = {'time': [], 'temp': [], 'hum': []}
+
+for once in thinned_data:
+    once_dict = json.loads(once)
+    list_data['temp'].append(round(float(once_dict['t']), 1))
+    list_data['hum'].append(round(float(once_dict['h']), 0))
+    list_data['time'].append(datetime.fromtimestamp(round(float(once_dict['time']), 0)).strftime("%H:%M:%S %d.%m.%Y"))
+
+
+#print(list_data)
+
+#plt.plot(list_data['time'], list_data['temp'], list_data['hum'])
+#plt.plot(list_data['time'], list_data['temp'])
+
+fig, ax = plt.subplots(2, 1, sharex=True)
+ax[0].plot(list_data['time'], list_data['temp'])
+ax[0].set_xlabel('time')
+ax[0].set_ylabel('temperature')
+
+ax[1].plot(list_data['time'], list_data['hum'])
+ax[1].set_xlabel('time')
+ax[1].set_ylabel('humidity')
+
+fig.tight_layout()
+fig.suptitle('temperature and humidity')
+plt.xticks(rotation=45, ha='right')
+plt.subplots_adjust(top=0.92, bottom=0.3, right=0.98)
+plt.show()
