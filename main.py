863517e6440b3b48301bd8a53cd8b37e9dfca1de
Ctyzan <osnovaa4tech@gmail.com>
2022-07-11 19:22:16 +0300

    add try exc to main

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ cooldown = 20
 
 if __name__ == '__main__':
     while True:
-        str_data = requests.get(url, timeout=10).text
-        data = json.loads(str_data)
-        if data['t'] != 'nan' and data['h'] != 'nan':
-            current_time = {'time': str(round(time.time(), 2))}
-            data.update(current_time)
+        try:
+            str_data = requests.get(url, timeout=10).text
+            data = json.loads(str_data)
+            if data['t'] != 'nan' and data['h'] != 'nan':
+                current_time = {'time': str(round(time.time(), 2))}
+                data.update(current_time)
 
-            with open('data.txt', mode='a') as f:
-                f.write(str(data) + '\n')
+                with open('data.txt', mode='a') as f:
+                    f.write(str(data) + '\n')
 
-            print(data)
+                print(data)
+        except:
+            pass
         time.sleep(cooldown)
